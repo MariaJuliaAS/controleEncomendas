34a9ad12e3c1f2e23818c3ca8cff5d23f8148a02
MariaJulia <mariajuliadev@gmail.com>
2025-09-28 15:35:20 -0300

    Responsividade da tabela

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -23,34 +23,47 @@ export function Home() {
                     </button>
                 </header>
 
-                <section className="bg-white w-full rounded-2xl border border-gray-200 shadow-md py-4 px-8 flex items-center justify-center max-h-130">
-                    <table className="w-full border-collapse table-fixed">
-                        <thead className="text-left">
-                            <tr className="border-b border-gray-200">
-                                <th className="pb-4">Nome</th>
-                                <th className="pb-4">Data de Entrega</th>
-                                <th className="pb-4">Endereço</th>
-                                <th className="pb-4">Contato</th>
-                                <th className="pb-4">Status</th>
-                                <th className="pb-4">Ações</th>
+                <section className="bg-white w-full rounded-2xl border border-gray-200 shadow-md px-8 py-4 flex max-h-9/12 overflow-y-auto">
+                    <table className="w-full border-collapse table-fixed block lg:table">
+                        <thead className="hidden lg:table-header-group text-left">
+                            <tr className="text-center border-b border-gray-200">
+                                <th className="pb-4 text-gray-500">Nome</th>
+                                <th className="pb-4 text-gray-500">Data de Entrega</th>
+                                <th className="pb-4 text-gray-500">Endereço</th>
+                                <th className="pb-4 text-gray-500">Contato</th>
+                                <th className="pb-4 text-gray-500">Status</th>
+                                <th className="pb-4 text-gray-500">Ações</th>
                             </tr>
                         </thead>
-                        <tbody id="tbody" className="text-left">
-                            <tr className="border-b border-gray-200 odd:bg-white even:bg-gray-100/30 transition-all hover:odd:bg-gray-100/10 hover:even:bg-gray-100/50">
-                                <td className="py-4">Maria Júlia</td>
-                                <td className="py-4">19/10/25</td>
-                                <td className="py-4">Rua das flores, n 10</td>
-                                <td className="py-4">84991832527</td>
-                                <td className="py-4">Para comprar</td>
-                                <td className="py-4 flex gap-2">
-                                    <FaUser size={20} className="text-zinc-800 cursor-pointer transition-all hover:scale-120" />
-                                    <FaEdit size={20} className="text-zinc-800 cursor-pointer transition-all hover:scale-120" />
-                                    <FaTrash size={20} className="text-zinc-800 cursor-pointer transition-all hover:scale-120" />
+                        <tbody className="text-left block lg:table-row-group">
+                            <tr className="border-b border-gray-200 odd:bg-white even:bg-gray-100/30 transition-all hover:odd:bg-gray-100/10 hover:even:bg-gray-100/50 block lg:table-row">
+                                <td data-label="Nome" className="text-zinc-800 py-4 px-2 block lg:table-cell lg:px-0 before:content-[attr(data-label)] before:font-medium before:text-gray-500 before:block lg:before:hidden before:float-left lg:text-center text-right">
+                                    Maria Júlia
+                                </td>
+                                <td data-label="Data de Entrega" className="text-zinc-800 py-4 px-2 block lg:table-cell lg:px-0 before:content-[attr(data-label)] before:font-medium before:text-gray-500 before:block lg:before:hidden before:float-left lg:text-center text-right">
+                                    19/10/25
+                                </td>
+                                <td data-label="Endereço" className="text-zinc-800 py-4 px-2 block lg:table-cell lg:px-0 before:content-[attr(data-label)] before:font-medium before:text-gray-500 before:block lg:before:hidden before:float-left lg:text-center text-right">
+                                    Rua das flores, n 10
+                                </td>
+                                <td data-label="Contato" className="text-zinc-800 py-4 px-2 block lg:table-cell lg:px-0 before:content-[attr(data-label)] before:font-medium before:text-gray-500 before:block lg:before:hidden before:float-left lg:text-center text-right">
+                                    84991832527
+                                </td>
+                                <td data-label="Status" className="text-zinc-800 py-4 px-2 block lg:table-cell lg:px-0 before:content-[attr(data-label)] before:font-medium before:text-gray-500 before:block lg:before:hidden before:float-left lg:text-center text-right">
+                                    Para comprar
+                                </td>
+                                <td data-label="Ações" className="py-4 px-2 block lg:table-cell lg:px-0 before:content-[attr(data-label)] before:font-medium before:text-gray-500 before:block lg:before:hidden before:float-left lg:text-center text-right">
+                                    <div className="flex gap-2 mt-2 lg:justify-center justify-end">
+                                        <FaUser size={20} className="text-zinc-600 cursor-pointer transition-all hover:scale-120" />
+                                        <FaEdit size={20} className="text-zinc-600 cursor-pointer transition-all hover:scale-120" />
+                                        <FaTrash size={20} className="text-zinc-600 cursor-pointer transition-all hover:scale-120" />
+                                    </div>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
                 </section>
+
             </main>
         </div>
     )
