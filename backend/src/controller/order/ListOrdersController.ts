faf5de9535c90c9938e7eee0b80f311f955e807c
MariaJulia <mariajuliadev@gmail.com>
2025-10-15 14:09:42 -0300

    Adicionando parametro para filtro de datas desc e asc

--- backend/src/controller/order/ListOrdersController.ts
+++ backend/src/controller/order/ListOrdersController.ts
@@ -3,8 +3,9 @@ import { ListOrdersService } from "../../service/order/ListOrdersService";
 
 class ListOrdersController {
     async handle(req: Request, res: Response) {
+        const order_by = req.query.order_by as string;
         const listOrdersService = new ListOrdersService();
-        const orders = await listOrdersService.execute();
+        const orders = await listOrdersService.execute({ order_by });
 
         return res.json(orders);
     }
--- backend/src/service/order/ListOrdersService.ts
+++ backend/src/service/order/ListOrdersService.ts
@@ -1,10 +1,14 @@
 import prismaClient from "../../prisma";
 
+interface ListOrderProps {
+    order_by: string;
+}
+
 class ListOrdersService {
-    async execute() {
+    async execute({ order_by }: ListOrderProps) {
         const orders = await prismaClient.order.findMany({
             orderBy: {
-                created_at: 'desc'
+                created_at: order_by === "desc" ? "desc" : "asc",
             },
             include: {
                 itens: true
